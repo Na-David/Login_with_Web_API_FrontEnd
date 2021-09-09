0bb9e7f6d63885076849436029263ca50a75274c
hna206@naver.com <hna206@naver.com>
2021-09-09 19:22:05 +1200

    Add project files.

---
+++ LOGIN/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace LOGIN
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText Username_Obj;
+        EditText Password_Obj;
+        Button btn_Login;
+        Button btn_SignUp;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Username_Obj = FindViewById<EditText>(Resource.Id.Username);
+            Password_Obj = FindViewById<EditText>(Resource.Id.Password);
+            btn_Login = FindViewById<Button>(Resource.Id.btnLogin);
+            btn_SignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
